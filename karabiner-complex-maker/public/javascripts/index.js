bc4157de3e8d665d2cdf559b1fd053fd6ca5eed7
shiraki_t <shiraki_t@example.com>
2018-06-19 13:28:10 +0900

    develop

--- karabiner-complex-maker/public/javascripts/index.js
+++ karabiner-complex-maker/public/javascripts/index.js
@@ -20,7 +20,7 @@ const createAddToKeyButton = $self => {
     $addButton.addClass('btn-primary');
     $addButton.addClass('col-sm-1');
     $addButton.text('Add');
-    $addButton.on('click', function() {
+    $addButton.on('click', function () {
         addToKey($(this));
     });
     return $addButton;
@@ -148,45 +148,228 @@ const toKeysOneline = ($elems) => {
     return toKeys.slice(' -> '.length);
 };
 
-const doneAdd = () => {
+const createModifySettingButton = () => {
+    const btn = $('button');
+    btn.addClass('modify-setting-btn');
+    btn.addClass('btn');
+    btn.addClass('btn-primary');
+    btn.text('Modify');
+    return btn;
+};
+
+const createRemoveSettingButton = () => {
+    const btn = $('button');
+    btn.addClass('remove-setting-btn');
+    btn.addClass('btn');
+    btn.addClass('btn-danger');
+    btn.text('Remove');
+    return btn;
+};
+
+const addManipulatorsRow = (description, from, optional, to, alone) => {
     const $th = $('<th>');
-    $th.prop('scope', 'row');
-    $th.text($('#description').val());
     const $td1 = $('<td>');
-    $td1.text($('#inputKey').val());
     const $td2 = $('<td>');
-    $td2.text('any');
     const $td3 = $('<td>');
-    $td3.text(toKeysOneline($('.to-key')));
     const $td4 = $('<td>');
-    $td4.text(toKeysOneline($('.to-key-alone')));
+    const $td5 = $('<td>');
+    const $td6 = $('<td>');
+    $th.prop('scope', 'row');
+    $th.text(description);
+    $td1.text(from);
+    $td2.text(optional);
+    $td3.text(to);
+    $td4.text(alone);
     const $tr = $('<tr>');
     $tr.append($th);
     $tr.append($td1);
     $tr.append($td2);
     $tr.append($td3);
     $tr.append($td4);
+    $tr.append($td5);
+    $tr.append($td6);
     $('#manipulators').append($tr);
+};
+
+const addSettingRow = () => {
+    addManipulatorsRow(
+        $('#description').val(),
+        $('#inputKey').val(),
+        'any',
+        toKeysOneline($('.to-key')),
+        toKeysOneline($('.to-key-alone'))
+    );
+};
+
+const doneAdd = () => {
+    addSettingRow();
     $.colorbox.close();
+    writeResult();
 };
 
 const cancelAdd = () => {
     $.colorbox.close();
 };
 
-const getResult = () => {
+const writeResult = () => {
+    const rowsData = [];
+    const rows = $('#manipulators').find(('tr'));
+    rows.each((i, row) => {
+        const rowData = {};
+        rowData.description = $(row).find('th').text();
+        const $cols = $(row).find('td');
+        rowData.from = $cols[0].textContent;
+        rowData.optional = $cols[1].textContent;
+        rowData.to = $cols[2].textContent;
+        rowData.alone = $cols[3].textContent;
+        rowsData.push(rowData);
+    });
+    const rowGroups = {};
+    rowsData.forEach(rowData => {
+        const manipulator = {};
+        manipulator.type = 'basic';
+        manipulator.from = {};
+        manipulator.from.key_code = '';
+        manipulator.from.modifiers = {};
+        let count = 0;
+        rowData.from.split(' ').reverse().forEach(k => {
+            if (!k) return;
+            ++count;
+            if (count === 1) {
+                return manipulator.from.key_code = k;
+            }
+            if (count === 2) {
+                manipulator.from.modifiers.mandatory = [];
+            }
+            manipulator.from.modifiers.mandatory.push(k);
+        });
+        if (rowData.optional) {
+            manipulator.from.modifiers.optional = [rowData.optional];
+        }
+        rowData.to.split(' -> ').forEach(to => {
+            if (!to) return;
+            let count = 0;
+            const toData = {};
+            to.split(' ').reverse().forEach(k => {
+                if (!k) return;
+                ++count;
+                if (count === 1) {
+                    return toData.key_code = k;
+                }
+                if (!toData.modifiers) {
+                    toData.modifiers = [];
+                }
+                toData.modifiers.push(k);
+            });
+            if (!manipulator.to) {
+                manipulator.to = [];
+            }
+            manipulator.to.push(toData);
+        });
+        rowData.alone.split(' -> ').forEach(alone => {
+            if (!alone) return;
+            let count = 0;
+            const aloneData = {};
+            alone.split(' ').reverse().forEach(k => {
+                if (!k) return;
+                ++count;
+                if (count === 1) {
+                    return aloneData.key_code = k;
+                }
+                if (!aloneData.modifiers) {
+                    aloneData.modifiers = [];
+                }
+                aloneData.modifiers.push(k);
+            });
+            if (!manipulator.to_if_alone) {
+                manipulator.to_if_alone = [];
+            }
+            manipulator.to_if_alone.push(aloneData);
+        });
+        if (!rowGroups[rowData.description]) {
+            rowGroups[rowData.description] = {};
+            rowGroups[rowData.description].manipulators = [];
+        }
+        rowGroups[rowData.description].manipulators.push(manipulator);
+    });
     const result = {};
     result.title = $('#title').val();
     result.rules = [];
-    const $tableData = $('#manipulators');
-    const rows = $tableData.find(('tr'));
-    rows.each((i, row) => {
-        const cols = $(row).find('td');
-        cols.each((i, col) => {
-            console.log(col);
+    for (let description in rowGroups) {
+        if (rowGroups.hasOwnProperty(description)) {
+            const rule = {};
+            rule.description = description;
+            rule.manipulators = rowGroups[description].manipulators;
+            result.rules.push(rule);
+        }
+    }
+    $('#result').val(JSON.stringify(result, undefined, 4));
+};
+
+const readExistingFile = (e) => {
+    const removeSpacePattern = /¥s?(.*)¥s?/;
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.addEventListener('load', () => {
+        const loadedData = JSON.parse(reader.result);
+        $('#title').val(loadedData.title);
+        loadedData.rules.forEach(rule => {
+            const description = rule.description;
+            rule.manipulators.forEach(manipulator => {
+                const from = manipulator.from;
+                const modifiers = from.modifiers;
+                let fromStr = '';
+                if (modifiers) {
+                    const mandatory = modifiers.mandatory;
+                    if (mandatory) {
+                        mandatory.forEach(k => {
+                            fromStr += k + ' ';
+                        });
+                    }
+                    fromStr += from.key_code;
+                }
+                const optional = modifiers.optional;
+                let optStr = '';
+                if (optional) {
+                    optional.forEach(op => {
+                        optStr += op + ' ';
+                    });
+                    if (optStr) {
+                        optStr = optStr.replace(removeSpacePattern, '$1');
+                    }
+                }
+                let toStr = '';
+                if (manipulator.to) {
+                    manipulator.to.forEach(to => {
+                        if (to.modifiers) {
+                            to.modifiers.forEach(mod => {
+                                toStr += mod + ' ';
+                            });
+                            toStr += to.key_code;
+                        }
+                        toStr += ' -> ';
+                    });
+                    toStr = toStr.slice(0, toStr.length - ' -> '.length);
+                }
+                let aloneStr = '';
+                if (manipulator.to_if_alone) {
+                    manipulator.to_if_alone.forEach(alone => {
+                        if (alone.modifiers) {
+                            alone.modifiers.forEach(mod => {
+                                aloneStr += mod + ' ';
+                            });
+                            aloneStr += alone.key_code;
+                        }
+                        aloneStr += ' -> ';
+                    });
+                    aloneStr = aloneStr.slice(0, ' -> '.length);
+                }
+                addManipulatorsRow(description, fromStr, optStr, toStr, aloneStr);
+            });
         });
+        $('#result').val(JSON.stringify(loadedData, undefined, 4));
     });
-    $('#result').val(JSON.stringify(result));
 };
 
 $(() => {
@@ -197,7 +380,7 @@ $(() => {
         overlayClose: false,
         escKey: false
     });
-    $('.add-to-key').on('click', function() {
+    $('.add-to-key').on('click', function () {
         addToKey($(this));
     });
     $('.add-to-key-alone').on('click', function () {
@@ -219,6 +402,9 @@ $(() => {
         cancelAdd();
     });
     $('#getResult').on('click', () => {
-        getResult();
+        writeResult();
     });
+    $('#fileRead').on('change', e => {
+        readExistingFile(e);
+    })
 });
